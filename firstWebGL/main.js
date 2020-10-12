7d9f3ed9e24996b9fdfc0a3a28331639253952ce
Michael Ricky <37214301+djtyranix@users.noreply.github.com>
2020-10-12 08:30:58 +0700

    Added code: 3 Points

--- firstWebGL/main.js
+++ firstWebGL/main.js
@@ -3,24 +3,25 @@ function main()
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
-    var vertexShaderCode = `
-    void main()
-    {
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-        gl_PointSize = 100.0;
-    }
-    `;
+    //Vertices of a Triangle
+    var vertices = [
+        -0.5, 0.5,      //Point A
+        -0.5, -0.5,     //Point B
+        0.5, -0.5       //Point C
+    ];
+
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var vertexShaderCode = document.getElementById("vertexShaderSource").text;
 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
 
-    var fragmentShaderCode = `
-    void main()
-    {
-        gl_FragColor = vec4(0.0, 0.803921568627451, 1.0, 1.0);
-    }
-    `;
+    var fragmentShaderCode = document.getElementById("fragmentShaderSource").text;
 
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
@@ -32,8 +33,16 @@ function main()
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    var aPos = gl.getAttribLocation(shaderProgram, "a_Pos");
+    gl.vertexAttribPointer(aPos, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPos);
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    var primitive = gl.POINTS;
+    var offset = 0;
+    var count = 3;
+    gl.drawArrays(primitive, offset, count);
 }
